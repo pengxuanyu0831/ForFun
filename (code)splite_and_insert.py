9509c6777f5a365ce29b1b54e0df98a1e71aa92c
pengxuanyu0831 <50193069+pengxuanyu0831@users.noreply.github.com>
2019-12-24 17:05:02 +0800

    commit for test

---
+++ (code)splite_and_insert.py
@@ -0,0 +1,7 @@
+filename = r'C:\Users\admin\Desktop\dxsj-20191203\pis\HOUSEINFO.txt'
+with open(filename,'r',encoding='utf-8')as f:
+    for line in f.readlines():
+        lines = str(line.replace("+","\n"))
+        path = r'C:\Users\admin\Desktop\test.txt'
+        g = open(path,'a')
+        g.write(lines)
