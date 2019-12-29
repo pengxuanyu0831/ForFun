3839eac46975f47ab72b214491184bb18fc2d44b
pengxuanyu0831 <50193069+pengxuanyu0831@users.noreply.github.com>
2019-12-29 20:52:25 +0800

    commit for test

--- (code)splite_and_insert.py
+++ (code)splite_and_insert.py
@@ -1,8 +1,17 @@
-filename = r'C:\Users\admin\Desktop\dxsj-20191203\pis\HOUSEINFO.txt'
+filename = r'C:\Users\admin\Desktop\dxsj-20191203\pis\PISTABLE0.txt'
+list = []
 with open(filename,'r',encoding='utf-8')as f:
     for line in f.readlines():
-        lines = str(line.replace("+","\n"))
+        lines = str(line.replace("+","\n").replace(",,,,,,,,",",").replace(",,,,",",").replace(",,,",",").replace(",,",","))
+        #lines_fina = lines.join("\n")
+        str1 = "insert into info_people values ("
+        str2 = ")"
+        str3 = ";"
+        str4 = "\n"
+        sql = str1 + lines + str2 + str3 + str4
         path = r'C:\Users\admin\Desktop\test.txt'
         g = open(path,'a')
-        g.write(lines)
-        print('已处理')
+        g.write(sql)
+
+
+
